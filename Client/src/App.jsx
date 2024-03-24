80ae90c4ea1882d5406b6d9aa8ccb81080e5a0a2
abdelfattahidr <abdelfattahidr122@gmail.com>
2024-03-24 15:41:18 +0000

    create hearder section

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,16 +5,18 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Dashbord from "./pages/Dashbord"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/sign-in" element={<Signin/>} />
-        <Route path="/sign-up" element={<Signup/>} />
-        <Route path="/dashbord" element={<Dashbord/>} />
-        <Route path="/projects" element={<Projects/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashbord" element={<Dashbord />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
