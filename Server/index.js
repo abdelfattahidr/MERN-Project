8e97330472fca60bb33a3b21536e084ae1c5dfcd
abdelfattahidr <abdelfattahidr122@gmail.com>
2024-03-24 21:46:21 +0000

    conect to database

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 const app = express();
-
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+     .then(() => {
+          console.log("mongoDB is connected")
+     })
+     .catch((err) => {
+          console.log(err)
+     })
 
 app.get('/test', (res, req) => {
      res.json("test ok")
