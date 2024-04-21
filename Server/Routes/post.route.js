ba28284cc98badb2c22f14ef55e06fe97738149a
abdelfattahidr <abdelfattahidr122@gmail.com>
2024-04-21 14:58:25 +0000

    create get posts aoi route

--- Server/Routes/post.route.js
+++ Server/Routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from "../utils/verifyUser.js"
-import { test,createPost } from "../Controllers/post.controller.js"
+import { test, createPost, getposts } from "../Controllers/post.controller.js"
 const router = express.Router()
 
 router.get('/test', test)
 router.post('/create', verifyToken, createPost)
+router.get('/getposts', getposts)
 
 export default router
