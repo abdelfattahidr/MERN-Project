2ff95052f063b7caee23c7f598386ed14005e43b
abdelfattahidr <abdelfattahidr122@gmail.com>
2024-05-04 00:41:42 +0000

    Add like functionality to the comment component

--- Server/Routes/comment.route.js
+++ Server/Routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { test, createCommet,getPostComments } from '../Controllers/comment.controller.js'
+import { test, createCommet, getPostComments, likeComment } from '../Controllers/comment.controller.js'
 import { verifyToken } from "../utils/verifyUser.js"
 const router = express.Router()
 
 router.get('/test', verifyToken, test)
 router.post('/create', verifyToken, createCommet)
 router.get('/getPostComments/:postId', verifyToken, getPostComments)
+router.put('/likeComment/:commentId', verifyToken, likeComment)
 
 export default router
